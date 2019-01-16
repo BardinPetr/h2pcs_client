dd4258207af21ccfe8b831ef66212985d6b89a39
Bardin Petr <bardin.petr@gmail.com>
2019-01-16 15:26:14 +0300

    Ecosystem changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,22 @@
 module.exports = {
-  apps: [{
-    name: "H2Pcs",
-    script: "index.js"
-  }],
+  apps: [
+    {
+      name: "H2Pcs",
+      script: "index.js"
+    }
+  ],
   deploy: {
     production: {
       key: "$HOME/.ssh",
       user: "pi",
-      host: ["192.168.1.44"],
+      host: ["192.168.43.129"],
       ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/master",
       repo: "https://bitbucket.org/BardinPetr/h2pcs-client",
       path: "/home/pi/h2pcs-client",
       "pre-deploy-local": "echo 'Deploy started'",
-      "post-deploy": "npm install",
-    },
+      "post-deploy":
+        "npm install; pm2 startOrRestart ecosystem.config.js; pm2 restart ecosystem.config.js --env production --update-env"
+    }
   }
-}
+};
