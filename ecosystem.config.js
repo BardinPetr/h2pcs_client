a1fa10244c2e49f6c285a25ce1cb5901e4e46703
BardinPetr <bardin.petr@gmail.com>
2020-03-07 18:31:53 +0300

    Ecosystem fixes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,9 +7,9 @@ module.exports = {
   ],
   deploy: {
     production: {
-      key: "$HOME/.ssh/id_rsa",
+      key: "~/.ssh/id_rsa.pub",
       user: "pi",
-      host: ["192.168.43.129"],
+      host: [process.env.DEPLOY_HOST | "192.168.43.129"],
       ssh_options: "StrictHostKeyChecking=no",
       ref: "origin/master",
       repo: "git@github.com:BardinPetr/h2pcs_client.git",
